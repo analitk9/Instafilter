eff3716845aa60ded25201b066974e4f938e6af0
Evdokimov Denis <analitik9@gmail.com>
2024-07-15 18:50:17 +0300

    Initial Commit + challenge

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Denis Evdokimov on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
